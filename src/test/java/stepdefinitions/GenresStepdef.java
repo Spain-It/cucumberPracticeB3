73c8f3a3998be25ae6c7fd3624d9feab545e9c83
Spain-It <tahsingokmen@hotmail.com>
2020-10-29 19:37:07 +0100

    updated

--- src/test/java/stepdefinitions/GenresStepdef.java
+++ src/test/java/stepdefinitions/GenresStepdef.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -17,12 +19,12 @@ public class GenresStepdef {
 
 
 
-    @Given("the user sends request {string} with end point")
+    @Given("the user sends post request {string} with end point")
 
     public void the_user_sends_request_with_end_point(String endPoint) {
 
         Map<String,String> genresData = new HashMap<>();
-        genresData.put("name","Science Fiction3");
+        genresData.put("name","Science Fiction1");
 
          movies.response = RestAssured.given().contentType(ContentType.JSON).when().
                 body(genresData).post(endPoint);
@@ -32,7 +34,46 @@ public class GenresStepdef {
         movies.response.prettyPrint();
 
         System.out.println(jsp.getString("name"));
+        System.out.println(jsp.getString("id"));
 
     }
 
+    @Given("the user sends get request with {string} endpoint")
+    public void the_user_sends_get_request_with_endpoint(String endPoint) {
+      movies.response = RestAssured.given().accept(ContentType.JSON).get(endPoint);
+
+      movies.response.prettyPrint();
+    }
+
+    @And("the user sends get request with {string} endpoint and {int} qyery")
+    public void theUserSendsGetRequestWithEndpointAndQyery(String endPoint, int idNum) {
+
+        movies.response = RestAssured.given().accept(ContentType.JSON).pathParam("id",idNum).when().
+                          get(endPoint+"/{id}");
+
+        movies.response.prettyPrint();
+    }
+
+    @When("the user sends put request with {string} endpoint")
+    public void the_user_sends_put_request_with_endpoint(String endPoint) {
+
+        Map<String,String> genresData = new HashMap<>();
+        genresData.put("name","Science Fiction10");
+
+        movies.response = RestAssured.given().accept(ContentType.JSON).when().
+                          body(genresData).put(endPoint+"/737");
+
+        movies.response.prettyPrint();
+
+
+
+    }
+
+    @When("the user sends delete request with {string} endpoint {int} pathParam")
+    public void theUserSendsDeleteRequestWithEndpointPathParam(String endPoint, int idNum) {
+
+        movies.response = RestAssured.given().pathParam("id",idNum).
+                         delete(endPoint+"/{id}");
+
+    }
 }
--- src/test/resources/features/Genres.feature
+++ src/test/resources/features/Genres.feature
@@ -1,6 +1,34 @@
 Feature: Post Genres End point
-  Scenario: Post
+  Background:
     Given the user set uri
-    And the user sends request "/genres" with end point
+
+  Scenario: Post
+
+    And the user sends post request "/genres" with end point
     When Verify the status code is 201
     And Verify the content type is "application/json; charset=utf-8"
+
+
+    Scenario: Get
+
+      And the user sends get request with "/genres" endpoint
+      When Verify the status code is 200
+      And Verify the content type is "application/json; charset=utf-8"
+
+      Scenario: Get with queryParam
+
+        And the user sends get request with "/genres" endpoint and 737 qyery
+        When Verify the status code is 200
+        And Verify the content type is "application/json; charset=utf-8"
+
+
+       Scenario: Put
+         When the user sends put request with "/genres" endpoint
+         When Verify the status code is 204
+
+
+         Scenario: delete
+           When the user sends delete request with "/genres" endpoint 735 pathParam
+           When Verify the status code is 204
+
+
